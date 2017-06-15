acc30b5622e2669071aa27e29037e4dd34cd35e0
Alex S. Rosa <alexsros@gmail.com>
2017-06-15 11:32:33 -0300

    photo register

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -7,5 +8,24 @@ import { Component, Input } from '@angular/core';
     templateUrl: './cadastro.component.html'
 })
 export class CadastroComponent{
+    
+    foto : FotoComponent = new FotoComponent();
+    http : Http;
 
+    constructor(http : Http){
+        this.http = http;
+    }
+
+    cadastrar() {
+        console.log(this.foto);
+
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http.post('v1/fotos', JSON.stringify(this.foto), { headers: headers })
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log('Foto salva com sucesso');
+            }, erro =>  console.log(erro));
+    }
 }
